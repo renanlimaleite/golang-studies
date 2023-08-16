dc0c396515b383869f19f35ef1adc786a41bbb6f
renanlimaleite <renan.lima.softwares@gmail.com>
2023-08-16 08:16:42 -0300

    manipulando arquivos

---
+++ 26-manipulando-arquivos/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("1. Escrevendo no arquivo com Write / WriteString...")
+	// tamanho, err := f.WriteString("Hello, World!")
+	tamanho, err := f.Write([]byte("Escrevendo dados no arquivo..."))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Arquivo criado com sucesso! Tamanho: %d bytes.\n", tamanho)
+	f.Close()
+
+	fmt.Println("2. Abrindo o arquivo e lendo com ReadFile")
+	arquivo, err := os.ReadFile("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(arquivo))
+
+	fmt.Println("3. Leitura de pouco em pouco abrindo o arquivo com bufio")
+	arquivo2, err := os.Open("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(arquivo2)
+	buffer := make([]byte, 10)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(buffer[:n])) // slice de 0 até n (10)
+	}
+
+}
